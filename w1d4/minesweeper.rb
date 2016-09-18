7f6cda8687bc22a53b91462c5efcf58561332543
andygiang <agiang363@gmail.com>
2016-09-17 18:03:44 -0700

    made recusion work stil need to render board nicely

--- w1d4/minesweeper.rb
+++ w1d4/minesweeper.rb
@@ -1,13 +1,13 @@
 class Tile
-  attr_accessor :value
-  attr_reader :revealed
+  attr_accessor :value,:flagged,:revealed
   def initialize(value=nil,revealed=false,flagged=false)
     @value = value
     @revealed = revealed
+    @flagged = flagged
   end
 end
 class Board
-  attr_reader :board
+  attr_accessor :board
    def initialize
      @board = Array.new(9) {Array.new(9) {Tile.new} }
    end
@@ -15,7 +15,7 @@ class Board
     10.times do
       x,y = rand(9),rand(9)
       until @board[x][y].value == nil
-         rand_pos = [rand(9),rand(9)]
+        x,y = rand(9),rand(9)
        end
        @board[x][y].value  = :bomb
      end
@@ -28,26 +28,35 @@ class Board
         all_pos << [x+i,y+j]
       end
     end
-    all_pos = all_pos.select {|x,y| x<9 and y<9 and x >=0 and y >=0}
+    all_pos = all_pos.reject {|x,y| x>=9 or y>=9 or x <0 or y <0}
   end
   def scatter_numbers
     for i in 0...9
       for j in 0...9
-        next if @board[x][y].value == :bomb
+        next if @board[i][j].value == :bomb
         neighbors = adjacent_tiles([i,j])
         count = 0
-        neighbors.each do |neighbor|
+        neighbors.each do |x,y|
           if @board[x][y].value == :bomb
             count+=1
           end
         end
-        @board[x][y].value = count
+        @board[i][j].value = count
       end
     end
   end
   def render
     @board.each do |row|
-      p row.collect {|tile| tile.value}
+      print_row = row.collect do |tile|
+        if tile.revealed == true
+          " #{tile.value}"
+        elsif tile.flagged == true
+          "flag"
+        else
+          nil
+        end
+      end
+      p print_row
     end
   end
   def win?
@@ -65,11 +74,11 @@ class Board
   def update_position
     x,y = prompt_pos
     flag = prompt_flag
-    if flag == 'safe'
-      @board[x][y].revealed == true
+    if flag == "safe"
+      @board[x][y].revealed = true
       wave([x,y])
     else
-      @board[x][y].flagged == true
+      @board[x][y].flagged = true
     end
   end
   def wave(pos)
@@ -79,18 +88,20 @@ class Board
     left = [x+1,y]
     right = [x-1,y]
     neighbors = [top,bottom,left,right]
-    return nil if neighbors.all? do |coor|
-      tile = @board[coor.first][coor.last]
-      tile.revealed == true or tile.value > 0 or tile.value == :bomb
-    end
-    if @board[x][y] == 0
-      neighbors.each do |coor|
-        @board[coor.first][coor.last].revealed == true
-        wave(coor)
+    neighbors.select! {|x,y| x<9 and y<9 and x >=0 and y >=0 and @board[x][y].revealed == false}
+    if @board[x][y].value == 0
+      neighbors.each do |i,j|
+        @board[i][j].revealed = true
+        wave([i,j]) if @board[i][j].value == 0
       end
     end
   end
-
+  def play
+    until win?
+      self.update_position
+      self.render
+    end
+  end
 end
 
 
@@ -98,5 +109,5 @@ a = Board.new
 a.render
 a.scatter_bombs
 a.scatter_numbers
-a.update_position
 a.render
+a.play
